ceb3bec33573f9c1ac13878913e4e3428c2aadbe
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-10-13 13:44:14 -0400

    first commit

---
+++ book.dart
@@ -0,0 +1,11 @@
+class Book {
+  Book(
+      {required this.title,
+      required this.author,
+      required this.ISBN,
+      required this.availabile});
+  final String title;
+  final String author;
+  final String ISBN;
+  final bool availabile;
+}
